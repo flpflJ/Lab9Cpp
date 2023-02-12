d5146166e2502f9da4776281ae40340187a7ab0e
Vsevolod <sevagoose@gmail.com>
2023-02-12 22:19:08 +0700

    IDK WHAT TO DO

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -45,8 +45,17 @@ public:
     void SetLen(int Arrlen) {
         this->len = Arrlen;
     }
+    void SetArray() {
+        mas = new Credit[len];
+    }
+    void SetArrThings(string inSur, string cName, string inSN, unsigned long long int cSum, double cstavka, int clengthYear, long int cpercentSum, int i) {
+        mas[i].SetAllThings(inSur, cName, inSN, cSum, cstavka, clengthYear, cpercentSum);
+    }
+    Credit getArr(int i) {
+        return mas[i];
+    }
     void SetAllThings(string inSurName, string inName, string inSecondName, unsigned long long int inSum, double inStavka, int inLen, long int inPer) {
-        this->Surname = Surname;
+        this->Surname = inSurName;
         this->Name = inName;
         this->SecondName = inSecondName;
         this->sum = inSum;
@@ -54,6 +63,7 @@ public:
         this->lengthYear = inLen;
         this->percentSum = inPer;
     }
+    
     string getSurname() {
         return Surname;
     }
@@ -76,40 +86,6 @@ public:
         return percentSum;
     }
 };
-
-void menu_for_sort() {
-    system("cls");
-    setlocale(LC_ALL, "Russian");
-    cout << "|+++++++++++++++++++++++++++++|" << endl;
-    cout << "|По какому столбцу отсортировать массив?|" << endl;
-    cout << "|1) Фамилия|" << endl;
-    cout << "|2) Имя|" << endl;
-    cout << "|3) Отчество|" << endl;
-    cout << "|4) Сумма|" << endl;
-    cout << "|5) Процентная ставка|" << endl;
-    cout << "|6) Срок кредита|" << endl;
-    cout << "|7) Переплата по кредиту|" << endl;
-    cout << "|+++++++++++++++++++++++++++++|" << endl;
-}
-
-void menu_print() {
-    system("cls");
-    setlocale(LC_ALL, "Russian");
-    cout << "|+++++++++++++++++++++++++++++|" << endl;
-    cout << "|Что вы хотите сделать?|" << endl;
-    cout << "|1) Ввод данных|" << endl;
-    cout << "|2) Вывести данные|" << endl;
-    cout << "|3) Отсортировать по полю|" << endl;
-    cout << "|4) Удалить один элемент|" << endl;
-    cout << "|5) Удалить все данные из класса(повтор программы)|" << endl;
-    cout << "|6) Записать данные в файл|" << endl;
-    cout << "|7) Выход|" << endl;
-    cout << "|+++++++++++++++++++++++++++++|" << endl;
-}
-
-void errmsg() {
-    cout << "Произошла ошибка при вводе данных, введено недопустимое значение. Повторите попытку." << endl;
-}
 
 unsigned long long int input_long() {
     unsigned long long int c = 0;
@@ -152,6 +128,40 @@ long int CreditPlatezh(unsigned long long int const sum, double const stavka, in
     long int platezh = x - sum;
     return platezh;
 
+}
+
+void menu_for_sort() {
+    system("cls");
+    setlocale(LC_ALL, "Russian");
+    cout << "|+++++++++++++++++++++++++++++|" << endl;
+    cout << "|По какому столбцу отсортировать массив?|" << endl;
+    cout << "|1) Фамилия|" << endl;
+    cout << "|2) Имя|" << endl;
+    cout << "|3) Отчество|" << endl;
+    cout << "|4) Сумма|" << endl;
+    cout << "|5) Процентная ставка|" << endl;
+    cout << "|6) Срок кредита|" << endl;
+    cout << "|7) Переплата по кредиту|" << endl;
+    cout << "|+++++++++++++++++++++++++++++|" << endl;
+}
+
+void menu_print() {
+    system("cls");
+    setlocale(LC_ALL, "Russian");
+    cout << "|+++++++++++++++++++++++++++++|" << endl;
+    cout << "|Что вы хотите сделать?|" << endl;
+    cout << "|1) Ввод данных|" << endl;
+    cout << "|2) Вывести данные|" << endl;
+    cout << "|3) Отсортировать по полю|" << endl;
+    cout << "|4) Удалить один элемент|" << endl;
+    cout << "|5) Удалить все данные из класса(повтор программы)|" << endl;
+    cout << "|6) Записать данные в файл|" << endl;
+    cout << "|7) Выход|" << endl;
+    cout << "|+++++++++++++++++++++++++++++|" << endl;
+}
+
+void errmsg() {
+    cout << "Произошла ошибка при вводе данных, введено недопустимое значение. Повторите попытку." << endl;
 }
 
 /*void ManualEntry(Credit* m, int& length) {
@@ -201,8 +211,27 @@ void ManualEntry(Credit m, int& length) {
     length = input();
     cin.ignore(255, '\n');
     m.SetLen(length);
-    m.SetArrThings();
-
+    m.SetArray();
+    for (int i = 0; i < length; i++) {
+        int x = i + 1;
+        cout << "Ввод строки номер " << x << endl;
+        cout << "Введите фамилию" << endl;
+        cin >> Surname;
+        cout << "Введите имя" << endl;
+        cin >> Name;
+        cout << "Введите отчество" << endl;
+        cin >> SecondName;
+        cout << "Введите сумму кредита" << endl;
+        sum = input_long();
+        cout << "Введите процентную ставку в год" << endl;
+        stavka = inputDouble();
+        cout << "Введите срок кредита(в годах)" << endl;
+        lengthYear = input();
+        percentSum = CreditPlatezh(sum, stavka, lengthYear);
+        cin.clear();
+        cin.sync();
+        m.SetArrThings(Surname, Name, SecondName, sum, stavka, lengthYear, percentSum, i);
+    }
 }
 
 
@@ -211,4 +240,7 @@ int main()
     int n = 1;
     Credit m;
     ManualEntry(m,n);
+    int d = 0;
+    Credit s = m.getArr(d);
+    cout << s.getName();
 }
